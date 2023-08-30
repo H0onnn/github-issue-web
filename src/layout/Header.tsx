0072727b87b1fc1918b4f403445fb1eb54680e03
H0onnn <dukei2017@naver.com>
2023-08-30 17:27:09 +0900

    Feat : repoData 로직 커스텀 훅으로 분리 및 헤더에 적용

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,25 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Endpoints } from '@octokit/types';
+import useRepoData from '../hooks/useRepoData';
 import { styled } from 'styled-components';
-import getRepoData from '../api/repoData';
 import { colors } from '../constants/colors';
 
 const Header = () => {
-  const [repoData, setRepoData] = useState<
-    Endpoints['GET /repos/{owner}/{repo}']['response']['data'] | null
-  >(null);
-
-  useEffect(() => {
-    const fetchRepoData = async () => {
-      try {
-        const data = await getRepoData('facebook', 'react');
-        setRepoData(data);
-      } catch (error: any) {
-        console.error('RepoData를 가져오는데 실패했습니다. :', error.message);
-      }
-    };
-    fetchRepoData();
-  }, []);
+  const repoData = useRepoData();
 
   return (
     <HeaderLayout>
