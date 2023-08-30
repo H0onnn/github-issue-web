e2c0b112925285169b971406f7c2cb9c7ada5d86
H0onnn <dukei2017@naver.com>
2023-08-30 21:34:17 +0900

    Design : 헤더 컴포넌트 css 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -21,6 +21,10 @@ const HeaderLayout = styled.div`
   width: 100%;
   height: 100px;
   background-color: ${colors.primary};
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
 `;
 
 const HeaderTitle = styled.h1`
