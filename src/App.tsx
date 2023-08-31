52895ea6ceb0090dfe1c357ce1f2acc398ab9441
H0onnn <dukei2017@naver.com>
2023-08-31 14:49:34 +0900

    Feat : App 컴포넌트 에러 및 로딩상황 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import PageLayout from './layout/PageLayout';
 import AppRoutes from './routes';
+import ErrorPage from './pages/ErrorPage';
+import { useIssueData } from './hooks/useIssueData';
+import LoadingComponent from './components/UI/Loading/LoadingComponent';
 
 const App = () => {
+  const { error, isError, loading } = useIssueData();
+
   return (
-    <Router>
+    <HashRouter>
       <PageLayout>
-        <AppRoutes />
+        {isError ? (
+          <ErrorPage message={error?.message} code={error?.code} />
+        ) : (
+          <>
+            {loading && <LoadingComponent />}
+            <AppRoutes />
+          </>
+        )}
       </PageLayout>
-    </Router>
+    </HashRouter>
   );
 };
 
