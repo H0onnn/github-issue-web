266cf1467f9d99a85d068a54938eeb3a2f3c290e
H0onnn <dukei2017@naver.com>
2023-08-31 14:55:48 +0900

    Feat : 라우터 경로 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,14 @@
-import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import IssueListPage from './pages/IssueListPage';
+import ErrorPage from './pages/ErrorPage';
+import IssueDetailPage from './pages/IssueDetailPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" Component={IssueListPage} />
+      <Route path="/issue/:id" Component={IssueDetailPage} />
+      <Route path="*" Component={ErrorPage} />
     </Routes>
   );
 };
