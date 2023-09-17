6f7fbf4c08db9baf92ebae0d9358eba9f6cc40c4
H0onnn <dukei2017@naver.com>
2023-09-17 20:27:29 +0900

    Feat : 루트 경로에선 헤더의 뒤로가기 버튼을 렌더링 하지 않음

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,22 +1,27 @@
 import useRepoData from '../hooks/useRepoData';
 import { styled } from 'styled-components';
 import { colors } from '../constants/colors';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '../components/UI/Button';
 
 const Header = () => {
   const repoData = useRepoData();
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <HeaderLayout>
-      <div style={{ padding: '20px', width: '100px' }}></div>
+      <EmptySpace />
 
       <HeaderTitle>{repoData ? `${repoData.owner.login} / ${repoData.name}` : null}</HeaderTitle>
 
-      <HeaderButton>
-        <Button onClick={() => navigate('/')}>Go Back</Button>
-      </HeaderButton>
+      {location.pathname === '/' ? (
+        <EmptySpace />
+      ) : (
+        <HeaderButton>
+          <Button onClick={() => navigate('/')}>Go Back</Button>
+        </HeaderButton>
+      )}
     </HeaderLayout>
   );
 };
@@ -51,4 +56,9 @@ const HeaderButton = styled.div`
   & > button:hover {
     color: ${colors.primary};
   }
+`;
+
+const EmptySpace = styled.div`
+  width: 100px;
+  padding: 20px;
 `;
