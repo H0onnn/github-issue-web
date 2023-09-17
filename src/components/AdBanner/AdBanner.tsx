c0af5a291e9f3f99be5e1529b36a0cc260134d42
H0onnn <dukei2017@naver.com>
2023-09-17 20:12:18 +0900

    Feat : 광고 컴포넌트 문구 추가 및 반응형 적용

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -7,8 +7,8 @@ const AdBanner = () => {
         <img
           src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
           alt="광고 배너"
-          style={{ width: '100%', height: '100px', objectFit: 'cover', cursor: 'pointer' }}
         />
+        <h3>(광고) 모두가 나답게 일하고 즐겁게 성장할 수 있도록</h3>
       </a>
     </AdBannerContainer>
   );
@@ -23,4 +23,10 @@ const AdBannerContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 10px;
+
+  @media (max-width: 768px) {
+    h3 {
+      display: none;
+    }
+  }
 `;
